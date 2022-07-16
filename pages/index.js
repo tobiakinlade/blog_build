47a77bf73ee108ec6264d957dc48e0b28418030f
tobiakinlade <tobi.akinlade@yahoo.com>
2022-07-16 15:58:47 +0100

    showing post from sanity in the UI

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { client } from 'lib/sanity'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='bg-gray-200 min-h-screen'>
       <Head>
@@ -16,25 +16,46 @@ export default function Home() {
         </h1>
 
         <div className='mt-20 mx-auto text-center max-w-3xl px-10'>
-          <div className='mb-10 p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col'>
-            <p className='mb-6 text-gray-400 uppercase text-sm'>Feb 10 2022</p>
-            <h3 className='text-3xl font-semibold text-gray-900'>
-              February big update
-            </h3>
-            <p className='mt-6 text-gray-500'>
-              Today we introduced a new big update ...
-            </p>
-          </div>
+          {posts.map((post, index) => (
+            <div
+              key={index}
+              className='mb-10 p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col'
+            >
+              <p className='mb-6 text-gray-400 uppercase text-sm'>
+                {new Date(post.publishedAt).toDateString().slice(4)}
+              </p>
+              <h3 className='text-3xl font-semibold text-gray-900'>
+                {post.title}
+              </h3>
+              {/* <p className='mt-6 text-gray-500'>
+                Today we introduced a new big update ...
+              </p> */}
+            </div>
+          ))}
 
-          <div className='mb-10 p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col'>
+          {/* <div className='mb-10 p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col'>
             <p className='mb-6 text-gray-400 uppercase text-sm'>Jan 06 2022</p>
             <h3 className='text-3xl font-semibold text-gray-900'>Some news</h3>
             <p className='mt-6 text-gray-500'>
               Today we introduced a new big update ...
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts =
+    await client.fetch(`*[_type == "post"] | order(publishedAt desc){
+    _id,
+    title,
+    publishedAt,
+    'slug': slug.current
+  }`)
+
+  return {
+    props: { posts },
+  }
 }
